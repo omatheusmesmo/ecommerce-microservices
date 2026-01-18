f88ac3a1b94a7c4fcd6ab919a260aff15cdbeb31
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-18 15:52:08 -0300

    chore: add NotificationType enum for notification type management

---
+++ notification-service/src/main/java/com/ecommerce/enums/NotificationType.java
@@ -0,0 +1,12 @@
+package com.ecommerce.enums;
+
+public enum NotificationType {
+    ORDER_CREATED,
+    ORDER_STATUS_CHANGED,
+    ORDER_CANCELLED,
+    ORDER_DELIVERED,
+    PRODUCT_CREATED,
+    STOCK_LOW_ALERT,
+    STOCK_OUT_ALERT,
+    STOCK_RESTOCKED
+}
