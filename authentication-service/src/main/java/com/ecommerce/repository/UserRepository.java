5468f3151825b446a275dedfc726f41d5da83b05
Matheus Oliveira <hi@omatheusmesmo.dev>
2026-01-28 18:49:37 -0300

    feat: add UserRepository with method to find user by email

---
+++ authentication-service/src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepository<User> {
+
+    public User findByEmail(String email){
+        return find("email", email).firstResult();
+    }
+}
